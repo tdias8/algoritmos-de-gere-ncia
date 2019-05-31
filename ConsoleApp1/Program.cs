e9b0611e7f6f6ddfc140a2651e9c61f2d32babd7
Tiago Dias <tdias8@hotmail.com>
2019-05-30 21:18:36 -0300

    first commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int menu;
+            Console.WriteLine("Selecione um algoritmo de escalonamento");
+            Console.WriteLine("1 - first-fit ");
+            Console.WriteLine("2 - circular-fit");
+            Console.WriteLine("3 - best-fit");
+            Console.WriteLine("4 - worst-fit");
+            menu = int.Parse(Console.ReadLine());
+
+            switch (menu)
+            {
+                case 1:
+                    
+                break;
+
+                case 2:
+                    
+                break;
+                
+                case 3
+                    
+                case 4
+                                
+
+            }
+        }
+    }
+}
